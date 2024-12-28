d5d574da4d08daea8bc3e95e08820ffa2004701e
hasanuyduran <hasanuyduran8080@gmail.com>
2024-12-28 11:49:50 +0300

    api connections for login page

--- src/Login.js
+++ src/Login.js
@@ -1,22 +1,34 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import axios from "axios";
+import api from "./Api";
 
 function Login() {
-  // useForm hook'unu kullanarak form kontrollerini alıyoruz
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  // Form gönderimi
-  const onSubmit = (data) => {
-    console.log(data); // Form verilerini konsola yazdır
-    alert("Form gönderildi!");
+  const onSubmit = async (data) => {
+    try {
+      const response = await api.post("/login", {
+        username: data.username,
+        password: data.password,
+      });
+
+      // Giriş başarılı olduğunda yapılacak işlemler
+      console.log("Giriş başarılı:", response.data);
+      // Örnek: Token saklama
+      localStorage.setItem("token", response.data.token);
+    } catch (error) {
+      // Hata durumunda yapılacak işlemler
+      console.error("Giriş başarısız:", error.response?.data || error.message);
+    }
   };
 
   return (
-    <div className="bg-gradient-to-r from-[#128C7E] via-[#25D366]  to-[#DCF8C6] animate-gradientShift bg-[length:200%_200%] w-full h-screen flex justify-center items-center">
+    <div className="bg-gradient-to-r from-[#128C7E] via-[#25D366]  to-[#c4f1a1] animate-gradientShift bg-[length:200%_200%] w-full h-screen flex justify-center items-center">
       <div className="w-1/3 h-1/2 bg-[#00000049] rounded-xl border-2  flex flex-col justify-center p-5">
         <div><h1 className="text-3xl font-black">LOGIN</h1></div>
         
@@ -24,10 +36,10 @@ function Login() {
           {/* Ad alanı */}
           <div className="w-full my-10" >
             <input className="w-full text-4xl text-center rounded-xl h-16"
-              id="name"
-              {...register("name", { required: "Ad zorunludur" })}
+              id="username"
+              {...register("username", { required: "Ad zorunludur" })}
             />
-            {errors.name && <p className="text-end text-red-600">{errors.name.message}</p>}
+            {errors.username && <p className="text-end text-red-600">{errors.username.message}</p>}
           </div>
 
           {/* E-posta alanı */}
